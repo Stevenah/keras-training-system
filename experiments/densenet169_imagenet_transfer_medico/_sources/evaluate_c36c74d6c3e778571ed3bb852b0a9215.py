9443e501b13f5e112f151e0b440235ae7b35685d
Steven Hicks <shicks1000@gmail.com>
2018-09-21 12:57:15 +0200

    update

---
+++ experiments/densenet169_imagenet_transfer_medico/_sources/evaluate_c36c74d6c3e778571ed3bb852b0a9215.py
@@ -0,0 +1,162 @@
+from utils.util import get_sub_dirs, pad_string
+from utils.metrics import *
+from utils.logging import *
+
+from scipy.misc import imread, imsave, imresize
+
+import tensorflow as tf
+import numpy as np
+import os
+import time
+
+# file paths
+kfold_split_file_path = ''
+
+
+def evaluate(model, config, experiment, validation_directory, file_identifier=''):
+
+    missclassified = {}
+
+    # get number of classes in model
+    number_of_classes = config['dataset']['number_of_classes']
+
+    # image dimensions
+    image_width = config['image_processing']['image_width']
+    image_height = config['image_processing']['image_height']
+    image_channels = config['image_processing']['image_channels']
+
+    # get class directory names from validation directory
+    class_names = get_sub_dirs(validation_directory)
+    class_names.sort()
+
+    # get keras labels in label-index format
+    label_index = { class_name: index for index, class_name in enumerate(class_names) }
+    index_label = { index: class_name for index, class_name in enumerate(class_names) }
+
+    # prepare confusion table
+    confusion = np.zeros((number_of_classes, number_of_classes))
+
+    # iterate over each class name
+    for class_name in class_names:
+        print(f'Starting {class_name}')
+
+        # set path to class directory
+        class_dir = os.path.join(validation_directory, class_name)
+
+        # iterate over each image in class directory
+        for file_name in os.listdir(class_dir):
+
+            # models class prediction for image
+            prediction = None
+
+            # process image before passing it through the network
+            image = imread(os.path.join(class_dir, file_name), mode='RGB')
+            image = imresize(image, (image_width, image_height, image_channels))
+            image = image.reshape(1, image_width, image_height, image_channels)
+            image = np.true_divide(image, 255.)
+
+            with tf.get_default_graph().as_default():
+                predictions = model.predict(image)[0]
+                prediction = np.argmax(predictions)
+
+            # check prediction against ground truth, i.e, if it equals the class directory name
+            if (prediction != label_index[class_name]):
+
+                # initialize empty list of fist missclassified of class
+                if class_name not in missclassified:
+                    missclassified[class_name] = { }
+                
+                missclassified[class_name][file_name] = {
+                    'prediction': index_label[prediction],
+                    'predictions': { index_label[class_index]: pred in enumerate(predictions) }
+                }
+
+            # update confusion table
+            confusion[prediction][label_index[class_name]] += 1
+
+    # calculate FP, FN, TP and TN based on confusion table
+    FP = confusion.sum(axis=0) - np.diag(confusion)  
+    FN = confusion.sum(axis=1) - np.diag(confusion)
+    TP = np.diag(confusion)
+    TN = confusion.sum() - (FP + FN + TP)
+
+    print ( f"True Positives: { TP }" )
+    print ( f"True Negatives: { TN }" )
+    print ( f"False Positives: { FP }" )
+    print ( f"False Positives: { FN }" )
+
+    # calculate metrics based on FP, FN, TP and TN
+    f1 = np.nan_to_num(f1score(TP, TN, FP, FN))
+    rec = np.nan_to_num(recall(TP, TN, FP, FN))
+    acc = np.nan_to_num(accuracy(TP, TN, FP, FN))
+    prec = np.nan_to_num(precision(TP, TN, FP, FN))
+    spec = np.nan_to_num(specificity(TP, TN, FP, FN))
+    mcc = np.nan_to_num(matthews_correlation_coefficient(TP, TN, FP, FN))
+
+    # bundle metrics into dictionary
+    metrics = { 'FP': FP, 'FN': FN, 'TP': TP, 'TN': TN, 'f1': f1, 'rec': rec, 'acc': acc, 'prec': prec, 'spec': spec, 'mcc': mcc }
+
+    # save missclassified images to file together with class
+    for class_name in missclassified:
+        log_misclassifications( f'{file_identifier}_class_misclassifications.txt', missclassified[class_name], class_name, index_label )
+
+    # write kvasir legend to results file
+    log_class_legend(f'{file_identifier}_split_evaluation_summary.txt', class_names)
+
+    # write confusion table to results file
+    log_confusion_table(f'{file_identifier}_split_evaluation_summary.txt', confusion)
+
+    # write model summary to results file
+    log_model_results(f'{file_identifier}_split_evaluation_summary.txt', metrics, file_identifier)
+
+    # write summaries for each class
+    for class_name in class_names:
+
+        # class index
+        class_index = label_index[class_name]
+        class_metrics = { key: value[class_index] for key, value in metrics.items() }
+
+        # write class summary to results file
+        log_class_results( f'{file_identifier}_class_results.txt', class_metrics, class_name, class_index)
+
+    evaluation_path = config['evaluation']['path']
+
+    print ("starting test validation...")
+
+    for file_name in os.listdir(evaluation_path):
+
+        prediction = None
+        prediction_time = None
+
+        image = imread(os.path.join(evaluation_path, file_name), mode='RGB')
+        image = imresize(image, (image_width, image_height, image_channels))
+        image = image.reshape(1, image_width, image_height, image_channels)
+        image = np.true_divide(image, 255.)
+
+        with tf.get_default_graph().as_default():
+            start_time = time.time()
+            prediction = model.predict(image)[0]
+            prediction_time = time.time() - start_time
+
+        prediction_index = np.argmax(prediction)
+        prediction_label = index_label[prediction_index]
+        
+        log_file_evaluation( f'{file_identifier}_test_evaluation_results.txt', file_name, prediction_label, prediction[prediction_index], prediction_time )
+
+    # add evaluation files to experiment
+    experiment.add_artifact( f'../tmp/{file_identifier}_split_evaluation_summary.txt' )
+    experiment.add_artifact( f'../tmp/{file_identifier}_class_misclassifications.txt' )
+    experiment.add_artifact( f'../tmp/{file_identifier}_class_results.txt' )
+    experiment.add_artifact( f'../tmp/{file_identifier}_test_evaluation_results.txt' )
+
+    # return evaluation metrics
+    return {
+        'f1': np.mean(f1),
+        'rec': np.mean(rec),
+        'acc': np.mean(acc),
+        'prec': np.mean(prec),
+        'spec': np.mean(spec),
+        'mcc': np.mean(mcc)
+    }
+
+            
---
+++ experiments/densenet169_imagenet_transfer_medico/_sources/train_90477d7bdbc2406bf0dde89463899cce.py
@@ -0,0 +1,195 @@
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint, EarlyStopping, Callback, TensorBoard
+
+from utils.builders import build_optimizer
+from utils.plotters import plot_accuracy, plot_loss
+
+import tensorflow as tf
+import numpy as np
+
+import importlib
+import shutil
+import math
+import os
+
+def create_class_weight(labels_dict, mu=0.20):
+
+    total = max (labels_dict.values())
+
+    class_weight = dict()
+
+    for key in labels_dict.keys():
+        print(f'{key}: {labels_dict[key]}')
+        score = 2 - float(labels_dict[key]) / total
+        class_weight[key] = score
+
+    return class_weight
+
+# function for training a model given a configuration
+def train( model, config, experiment, training_directory=None,
+    validation_directory=None, file_identifier=None ):
+
+    if training_directory is None:
+        training_directory = config['dataset']['training_directory']
+    
+    if validation_directory is None:
+        validation_directory = config['dataset']['validation_directory']    
+
+    # get file path
+    weights_file = f'{file_identifier}_{config["experiment"]["name"]}_weights.h5'
+    model_file = f'{file_identifier}_{config["experiment"]["name"]}_model.h5'
+
+    # get plot file names
+    accuracy_plot_file = f'{file_identifier}_accuracy_plot.png'
+    loss_plot_file = f'{file_identifier}_loss_plot.png'
+
+    # plot file paths
+    accuracy_plot_path = os.path.join('../tmp', accuracy_plot_file)
+    loss_plot_path = os.path.join('../tmp', loss_plot_file)
+
+    # weights/model file paths
+    model_path = os.path.join('../tmp', model_file)
+    weights_path = os.path.join('../tmp', weights_file)
+
+    # set patience (number of epochs where val_acc does not improve before stopping)
+    patience = config['hyper_parameters'].get('patience', 0)
+    baseline = config['hyper_parameters'].get('baseline', 0)
+    monitor = config['hyper_parameters'].get('monitor', 'val_acc')
+
+    # callbacks to be called after every epoch
+    callbacks = [
+        ModelCheckpoint(weights_path, monitor=monitor, verbose=1, save_best_only=True),
+        TensorBoard(log_dir=os.path.join('../tmp', 'logs'), batch_size=8)
+    ]
+
+    # number of splits in dataset
+    split = config['dataset']['split']
+
+    # number of samples in dataset
+    samples = config['dataset']['samples']
+
+    # metrics to measure under training
+    metrics = [ 'accuracy' ] 
+
+    # set number of  training and validation samples
+    training_samples = samples - (samples // split) # number of training samples in dataset
+    validation_samples = samples // split # number of validation samples in dataset
+
+    # set number of epochs
+    epochs = config['hyper_parameters']['epochs']
+
+    # set batch size
+    batch_size = config['hyper_parameters']['batch_size']
+
+    # set number of layers to freeze (not train)
+    freeze_layers = config['hyper_parameters']['freeze_layers']
+
+    # number of classes in dataset
+    number_of_classes = config['dataset']['number_of_classes']
+
+    # set image dimensions
+    image_width = config['image_processing']['image_width'] # change based on the shape/structure of your images
+    image_height = config['image_processing']['image_height'] # change based on the shape/structure of your images
+    image_channels = config['image_processing']['image_channels'] # number of image channels
+
+    # set training steps based on training sampels and batch size
+    training_steps = training_samples // batch_size   # number of training batches in one epoch
+    validation_steps = validation_samples // batch_size # number of validation batches in one epoch
+
+    # build optimizer
+    optimizer = build_optimizer(config['optimizer'])
+
+    training_generator_file = config['image_processing']['training_data_generator']
+    validation_generator_file = config['image_processing']['validation_data_generator']
+
+    # build data generators
+    training_data_generator = importlib.import_module(f'generators.{training_generator_file}').train_data_generator
+    validation_data_generator = importlib.import_module(f'generators.{validation_generator_file}').validation_data_generator
+
+    # freeze layers based on freeze_layers parameter
+    for layer in model.layers[:freeze_layers]:
+        layer.trainable = False
+    for layer in model.layers[freeze_layers:]:
+        layer.trainable = True
+
+    # initialize training generator
+    training_generator = training_data_generator.flow_from_directory(
+        training_directory, target_size=(image_width, image_height),
+        batch_size=batch_size, class_mode='categorical', follow_links=True)
+
+    # initialize validation generator
+    validation_generator = validation_data_generator.flow_from_directory(
+        validation_directory, target_size=(image_width, image_height),
+        batch_size=batch_size, class_mode='categorical', follow_links=True)
+
+    # only set early stopping if patience is more than 0
+    if patience > 0:
+        
+        # append early stopping to callbacks
+        callbacks.append(EarlyStopping(
+            monitor=monitor,
+            patience=patience,
+            verbose=0,
+            baseline=baseline))
+
+    # print out the class indicies for sanity
+    print('train indicies: ', training_generator.class_indices)
+    print('validation indicies: ', validation_generator.class_indices)
+
+    # compile model
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer=optimizer,
+        metrics=metrics)
+
+    # difference in class counts
+    class_dispersion = { }
+
+    class_list = os.listdir(training_directory)
+    class_list.sort()
+
+    class_weigths = None
+
+    if config['hyper_parameters']['class_weights']:
+        for class_index, class_name in enumerate(class_list):
+            class_dispersion[class_index] = len(os.lsitdir(os.path.join(training_directory, class_name)))
+            
+        class_weigths = create_class_weight(class_dispersion)
+
+        for index, weight in class_weigths.items():
+            print(f'{class_list[index]}: ', weight)
+
+    # train model and get training metrics
+    history = model.fit_generator(training_generator,
+        steps_per_epoch=training_steps,
+        epochs=epochs,
+        validation_data=validation_generator,
+        validation_steps=validation_steps,
+        callbacks=callbacks,
+        workers=0,
+        use_multiprocessing=False,
+        class_weight=class_weigths)
+
+    # plot loss
+    plot_loss(history, loss_plot_path)
+
+    # plot accuracy
+    plot_accuracy(history, accuracy_plot_path)
+
+    # add plots to experiment
+    experiment.add_artifact(accuracy_plot_path)
+    experiment.add_artifact(loss_plot_path)
+
+    # load best weights
+    if os.path.isfile(weights_path):
+        model.load_weights(weights_path)
+        experiment.add_artifact(weights_path)
+
+    # save keras model file
+    model.save(model_path)
+
+    # add keras model file to experiment
+    experiment.add_artifact(model_path)
+
+    # return training history metrics
+    return model
